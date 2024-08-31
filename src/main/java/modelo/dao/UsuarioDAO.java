a1c4233fbf91808816419ca573354eaaf4e4f6f8
Juan Díaz <jchendiaz15@gmail.com>
2024-08-31 16:33:06 -0600

    nuevos metodos de buscarPin y modificarPin, cambios en el metodo modificar

--- src/main/java/modelo/dao/UsuarioDAO.java
+++ src/main/java/modelo/dao/UsuarioDAO.java
@@ -27,8 +27,19 @@ public class UsuarioDAO {
         return n;
     }
 
+    public int buscarPin(String PIN) {
+        int n = -1; // variable si no encontro al usuario
+        for (int i = 0; i < usuarios.size(); i++) { //recorre la lista
+            if (usuarios.get(i).getPIN().equals(PIN)) { //verifica en la lista si ya existe un usuario
+                n = i;
+                break;
+            }
+        }
+        return n;
+    }
+
     public boolean insertar(Usuario usuario) {
-        if (buscar(usuario.getNumTarjeta()) == -1) { // si el usuario no existe
+        if (buscar(usuario.getNumTarjeta()) == -1 && buscarPin(usuario.getPIN()) == -1) { // si el usuario no existe
             usuarios.add(usuario); //agrega al usuario
             return true;
         } else {
@@ -36,7 +47,46 @@ public class UsuarioDAO {
         }
     }
 
-    public boolean modificar(Usuario usuarioModificado) {
+    public boolean modificar(Usuario usuarioModificado, String numeroTarjetaOriginal) {
+        // Buscar al usuario original usando el número de tarjeta anterior
+        int indice = buscar(numeroTarjetaOriginal);
+
+        if (indice != -1) { // Si el usuario existe
+            // Obtener el usuario actual
+            Usuario usuarioActual = usuarios.get(indice);
+
+            // Actualizar los datos del usuario actual
+            usuarioActual.setNumCuenta(usuarioModificado.getNumCuenta());
+            usuarioActual.setNombre(usuarioModificado.getNombre());
+            usuarioActual.setPIN(usuarioModificado.getPIN());
+            usuarioActual.setSaldo(usuarioModificado.getSaldo());
+            usuarioActual.setMonto(usuarioModificado.getMonto());
+
+            // Si el número de tarjeta ha cambiado, realiza la actualización
+            if (!numeroTarjetaOriginal.equals(usuarioModificado.getNumTarjeta())) {
+                // Elimina el usuario original
+                usuarios.remove(indice);
+                Usuario usuarioNuevo = new Usuario();
+                // Actualiza el número de tarjeta
+                usuarioNuevo.setNumTarjeta(usuarioModificado.getNumTarjeta());
+                // Actualizar los datos del usuario actual
+                usuarioNuevo.setNumCuenta(usuarioModificado.getNumCuenta());
+                usuarioNuevo.setNombre(usuarioModificado.getNombre());
+                usuarioNuevo.setPIN(usuarioModificado.getPIN());
+                usuarioNuevo.setSaldo(usuarioModificado.getSaldo());
+                usuarioNuevo.setMonto(usuarioModificado.getMonto());
+
+                // Inserta el usuario con el nuevo número de tarjeta
+                usuarios.add(usuarioNuevo);
+            }
+
+            return true;
+        } else {
+            return false; // Si no se encuentra el usuario
+        }
+    }
+
+    public boolean modificarPin(Usuario usuarioModificado) {
         // Busca al usuario original usando su número de tarjeta actual antes de modificarlo
         String numeroTarjetaOriginal = usuarioModificado.getNumTarjeta();
         int indice = buscar(numeroTarjetaOriginal);
@@ -46,24 +96,7 @@ public class UsuarioDAO {
             Usuario usuarioActual = usuarios.get(indice);
 
             // Actualizar los datos
-            usuarioActual.setNombre(usuarioModificado.getNombre());
             usuarioActual.setPIN(usuarioModificado.getPIN());
-            usuarioActual.setSaldo(usuarioModificado.getSaldo());
-            usuarioActual.setMonto(usuarioModificado.getMonto());
-
-            // Si el número de tarjeta ha cambiado, realiza la actualización en la lista
-            if (!usuarioActual.getNumTarjeta().equals(usuarioModificado.getNumTarjeta())) {
-                String nuevoNumeroTarjeta = usuarioModificado.getNumTarjeta();
-
-                // Actualizar el número de tarjeta
-                usuarioActual.setNumTarjeta(nuevoNumeroTarjeta);
-
-                // Elimina el usuario original de la lista usando el índice
-                usuarios.remove(indice);
-
-                // Inserta el usuario modificado con el nuevo número de tarjeta en la posición correcta
-                usuarios.add(usuarioActual);
-            }
 
             return true;
         } else {
