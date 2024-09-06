b9579a83bdbe8b42ef7f5b8459123e87398db923
Juan Díaz <jchendiaz15@gmail.com>
2024-09-05 20:47:17 -0600

    Modificacion en el metodo de buscarUsuario

--- src/main/java/Interfaz/Consult_User.java
+++ src/main/java/Interfaz/Consult_User.java
@@ -178,9 +178,7 @@ public class Consult_User extends javax.swing.JPanel {
                 txtSaldoActual.setText("");
                 txtSaldoActual.setText(usuario.getSaldo());
                 txtUltimoAcceso.setText("");
-                if (usuario.getFechaHora_Salida() != null) {
-                    txtUltimoAcceso.setText(usuario.getFechaHora_Salida());
-                }
+                txtUltimoAcceso.setText(usuario.getFechaHora_Salida());
             } else {
                 JOptionPane.showMessageDialog(null, "El Usuario no existe");
             }
