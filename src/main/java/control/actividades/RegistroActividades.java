ab80e5bd9cb9aa87c80b512ce6e93f93653ae55a
Juan Díaz <jchendiaz15@gmail.com>
2024-09-15 17:09:49 -0600

    Clase para llevar el registro de actividades hechas en el programa

--- src/main/java/control/actividades/RegistroActividades.java
+++ src/main/java/control/actividades/RegistroActividades.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  *
  * @author Juan Diaz
  */
-public class Actividades_Usuario_Administrador {
+public class RegistroActividades {
 
     public static void registrarActividadUsuario(String actividad) {
         // Crear la carpeta si no existe
@@ -47,4 +47,57 @@ public class Actividades_Usuario_Administrador {
         }
     }
 
+    public static void registrarTransacciones(String actividad) {
+        // Crear la carpeta si no existe
+        File directory = new File("RegistroTransacciones");
+        if (!directory.exists()) {
+            directory.mkdirs(); // Crea la carpeta si no existe
+        }
+
+        // Archivo dentro de la carpeta "actividadesAdministrador"
+        File file = new File(directory, "transacciones.txt");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(actividad);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void registrarDepositos(String actividad) {
+        // Crear la carpeta si no existe
+        File directory = new File("RegistroDepositos");
+        if (!directory.exists()) {
+            directory.mkdirs(); // Crea la carpeta si no existe
+        }
+
+        // Archivo dentro de la carpeta "actividadesAdministrador"
+        File file = new File(directory, "depositos.txt");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(actividad);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void registrarRetiros(String actividad) {
+        // Crear la carpeta si no existe
+        File directory = new File("RegistroRetiros");
+        if (!directory.exists()) {
+            directory.mkdirs(); // Crea la carpeta si no existe
+        }
+
+        // Archivo dentro de la carpeta "actividadesAdministrador"
+        File file = new File(directory, "retiros.txt");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(actividad);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
