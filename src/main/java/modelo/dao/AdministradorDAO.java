91290a9e34db0d614b865d5ab4d89c5e5f97df57
Juan Díaz <jchendiaz15@gmail.com>
2024-09-15 17:11:58 -0600

    Metodos de carga de los administradores

--- src/main/java/modelo/dao/AdministradorDAO.java
+++ src/main/java/modelo/dao/AdministradorDAO.java
@@ -1,5 +1,9 @@
 package modelo.dao;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import modelo.beans.Administrador;
@@ -15,17 +19,42 @@ public class AdministradorDAO {
 
     public AdministradorDAO() {
         administradores = new ArrayList<>();
+        cargarAdministradores();
     }
 
+    // Cargar usuarios desde un archivo .txt
+    public void cargarAdministradores() {
+        File file = new File("datosAdministradores", "Administradores.txt");
+
+        if (file.exists()) {
+            administradores.clear();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Administrador administrador = Administrador.fromString(line);
+                    administradores.add(administrador);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("El archivo 'Administradores.txt' no existe en la carpeta 'datosAdministradores'.");
+        }
+    }
+    
+    //Metodo para registrar el acceso de los administradores
     public String registrarAcceso(Administrador admin) {
-        FechaHora fh = new FechaHora();
-        admin.setFechaAcceso(fh.FechaAcceso());
-        admin.setHoraAcceso(fh.HoraAcceso());
-        String acceso = admin.getFechaHora_Acceso();
-        return acceso;
+        if (admin != null) {
+            FechaHora fh = new FechaHora();
+            admin.setFechaAcceso(fh.FechaAcceso());
+            admin.setHoraAcceso(fh.HoraAcceso());
+            String acceso = admin.getFechaHora_Acceso();
+            return acceso;
+        }
+        return null;
     }
 
-    public int buscar(String usuario_admin) {
+    private int buscar(String usuario_admin) {
         int n = -1; // variable si no encontro al usuario
         for (int i = 0; i < administradores.size(); i++) { //recorre la lista
             if (administradores.get(i).getUsuario_admin().equals(usuario_admin)) { //verifica en la lista si ya existe un usuario
@@ -36,7 +65,7 @@ public class AdministradorDAO {
         return n;
     }
 
-    public boolean insertar(Administrador administrador) {
+    private boolean insertar(Administrador administrador) {
         if (buscar(administrador.getUsuario_admin()) == -1) { // si el usuario no existe
             administradores.add(administrador); //agrega al usuario
             return true;
@@ -45,7 +74,7 @@ public class AdministradorDAO {
         }
     }
 
-    public boolean modificar(Administrador administrador) {
+    private boolean modificar(Administrador administrador) {
         if (buscar(administrador.getUsuario_admin()) != -1) { // si el usuario existe
             Administrador adminaux = obtener(administrador.getUsuario_admin());
             adminaux.setUsuario_admin(administrador.getUsuario_admin());
@@ -56,7 +85,7 @@ public class AdministradorDAO {
         }
     }
 
-    public boolean eliminar(String administrador) {
+    private boolean eliminar(String administrador) {
         if (buscar(administrador) != -1) { // si el usuario existe
             administradores.remove(buscar(administrador));
             return true;
